1c2c088cfbe52d4fc0ee8f7e3874388a4ce35d95
Adrian Sieber <mail@adriansieber.com>
2016-02-20 20:02:54 +0100

    Enable filtering and sorting, use nconf for flags
    - Add development cli

--- source/index.js
+++ source/index.js
@@ -2,6 +2,19 @@ import path from 'path'
 
 import fsp from 'fs-promise'
 import yaml from 'js-yaml'
+import nconf from 'nconf'
+
+nconf
+	.argv()
+	.env()
+	.file(path.join(process.cwd(), '.gitisrc'))
+	.defaults({
+		sortBy: 'number',
+		sortOrder: 'ascending',
+		filter: {
+			state: 'open'
+		}
+	})
 
 let issuesPath = path.join(process.cwd(), 'issues')
 
@@ -24,6 +37,44 @@ fsp
 							path.extname(filePaths[index])
 						)
 						return json
+					})
+					.filter(issue => {
+						let filter = nconf.get('filter')
+
+						for (let filterName in filter) {
+							if (issue[filterName] !== filter[filterName])
+								return false
+						}
+						return true
+					})
+					.sort((issueA, issueB) => {
+						let sortBy = nconf.get('sortBy')
+						let sortOrder = nconf.get('sortOrder')
+						let sortValueA = issueA[sortBy]
+						let sortValueB = issueB[sortBy]
+						let value
+
+						if (typeof sortValueA === 'number') {
+							if (typeof sortValueB !== 'number')
+								sortValueB = Number(sortValueB)
+							else
+								value = sortValueA - sortValueB
+						}
+						else {
+							if (typeof sortValueB === 'number')
+								sortValueA = Number(sortValueA)
+							else
+								value = sortValueA.localeCompare(sortValueB)
+						}
+
+						if (sortOrder === 'ascending')
+							return value
+						else if (sortOrder === 'descending')
+							return -value
+						else
+							throw new Error(
+								sortOrder + ' is no supported sorting order'
+							)
 					})
 					.forEach(json =>
 						console.log(json.number + ': ' + json.title)
